d17e3470ec361cf05b8f29603f4ee22c58319f5c
Kyuuxen <121227859+Kyuuxen@users.noreply.github.com>
2024-12-09 12:13:25 +0800

    Update sample.js

--- modules/scripts/events/sample.js
+++ modules/scripts/events/sample.js
@@ -9,12 +9,12 @@ module.exports.config = {
 module.exports.run = async function({ event, args }) {
   // YOU CAN ADD ANYTHING HERE, ADD MORE OR DO WHAT YOU WANT HERE, I DON'T CARE ^-^
   
-  /** EVENT TYPES
-   * postback
-   * quick_reply
-   * message_reaction
-   * message_reply
-   * message
-   * mark_as_seen
-   * @YanMaglinte **/
+  /EVENT TYPES
+    postback
+    quick_reply
+    message_reaction
+    message_reply
+    message
+    mark_as_seen
+   /* @YanMaglinte **/
 };
